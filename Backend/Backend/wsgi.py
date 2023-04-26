bcef13085eff0f08d67d9c3f4efecceab43c57a0
Zeref <enricomiguelv@gmail.com>
2023-04-26 18:46:02 +0800

    allowed hosts

--- Backend/Backend/wsgi.py
+++ Backend/Backend/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for Backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+path = '/home/MrBreak2022/artifex'
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Backend.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
-
 application = get_wsgi_application()
