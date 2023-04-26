804596af892c8d1da540f3204e9319c67dddce77
Zeref <enricomiguelv@gmail.com>
2023-04-26 18:54:27 +0800

    commiitng

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m54t^wbjxy**z7s-0d%&y#xre2-0u)(ic-j*8-@_yp3y$3g=j!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/', 'mrbreak2022.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'mrbreak2022.pythonanywhere.com']
 
 
 # Application definition
